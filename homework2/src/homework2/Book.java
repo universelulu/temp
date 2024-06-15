1f01aec63d106866bbcbfa22aff8a82114505055
jangs <jangs@DESKTOP-R46BTDF>
2024-06-16 02:16:18 +0900

    first commit

---
+++ homework2/src/homework2/Book.java
@@ -0,0 +1,29 @@
+package homework2;
+
+public class Book {
+	
+	private int id;
+	private String title;
+	
+	public Book(int id, String title) // Book 클래스 생성자
+	{
+		this.id = id;
+		this.title = title;
+	}
+	
+	public int GetId() // id값 리턴
+	{
+		return id;
+	}
+	
+	public String GetTitle()
+	{
+		return title;
+	}
+	
+	public String toString()
+	{
+		return "{id: "+ id + ", title: " + title + "}";
+	}
+
+}
